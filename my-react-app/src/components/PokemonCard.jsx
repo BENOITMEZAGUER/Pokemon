799268282ddeb0ae3b082c8820a90e558f806b82
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-03-27 14:38:32 +0100

    nouveaux ajouts

--- my-react-app/src/components/PokemonCard.jsx
+++ my-react-app/src/components/PokemonCard.jsx
@@ -1,14 +1,11 @@
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+  console.log({ pokemon });
+
   return (
-    <div>
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="bulbasaur"
-        ></img>
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
-    </div>
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : "???"}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
   );
 }
 
