975cd22ec038534e515693c0567fc409b232af22
BENOITMEZAGUER <benoit.mezaguer@gmail.com>
2024-03-28 11:37:41 +0100

    quête 6

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const pokemon = pokemonList[pokemonIndex];
+
+  const handleClickPrevious = () => {
+    setpokemonIndex(pokemonIndex - 1);
+  };
+
+  const handleClickNext = () => {
+    setpokemonIndex(pokemonIndex + 1);
+  };
+
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[1]} />
+      <PokemonCard pokemon={pokemon} />
+      {pokemonIndex > 0 && (
+        <button onClick={handleClickPrevious}>Précédent</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={handleClickNext}>Suivant</button>
+      )}
+      ;
     </div>
   );
 }
@@ -15,8 +35,26 @@ const pokemonList = [
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
+
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+
   {
     name: "mew",
   },
 ];
+
 export default App;
